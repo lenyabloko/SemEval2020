dbcf7cf1a3c19bfe8fde373bab76c9c7ec4b63c2
Len <lenyabloko@gmail.com>
2020-02-29 14:34:38 -0500

    initial commit
    initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+pip3 install numpy
+pip3 install tqdm
+pip3 install torch
+pip3 install sklearn
+pip3 install pandas
+pip3 install transformers
+pip3 install pytorch-transformers
+pip3 install tensorboardX
